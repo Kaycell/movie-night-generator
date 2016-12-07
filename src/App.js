9adede235628877a0e1858ae1f0d0120a9d3116b
Candidat 202 <candidat.202@insolus.com>
2016-12-07 23:42:17 +0100

    Basic static version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import television from './television.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +7,43 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={television} className="App-logo" alt="logo" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Movie className="Movie"/>
+        <Meal className="Meal"/>
+        <Clothing className="Clothing"/>
       </div>
     );
+  }
+}
+
+class Movie extends Component {
+  render() {
+    return (
+      <div className="Movie">
+        <img src="https://www.movieposter.com/posters/archive/main/226/MPW-113357" className="MoviePoster" alt="movieposter" />
+      </div>
+    )
+  }
+}
+
+class Meal extends Component {
+  render() {
+    return (
+      <div className="Meal">
+        <p>A Meal</p>
+      </div>
+    )
+  }
+}
+
+class Clothing extends Component {
+  render() {
+    return (
+      <div className="Clothing">
+        <p>A Clothing</p>
+      </div>
+    )
   }
 }
 
